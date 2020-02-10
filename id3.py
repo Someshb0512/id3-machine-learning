cb5a2bc7596cd811a65dd09a6b62b5f27f538580
Raihan Luthfi Haryawan <raihan.luthfi.h@gmail.com>
2020-02-10 20:35:34 +0700

    initial commit

---
+++ id3.py
@@ -0,0 +1,13 @@
+# import numpy as np
+
+class id3:
+    def __init__(self):
+        #self.max_depth = max_depth
+        print("test")
+
+    # def fit(self, X, y, ):
+
+    def test(self, str):
+        print(str)
+        
+    
---
+++ test.py
@@ -0,0 +1,9 @@
+from id3 import id3
+
+def main():
+    test = id3()
+    test.test("jancok")
+
+if __name__ == "__main__":
+    main()
+    
