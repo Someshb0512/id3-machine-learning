2d22f6646f4cd03185f34140a55d85ccbb34102a
Raihan Luthfi Haryawan <raihan.luthfi.h@gmail.com>
2020-02-14 00:30:26 +0700

    add c45.py file and add handleMissingValue function

---
+++ c45.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+class c45:
+    def __init__(self):
+        self.root = Node('DTL')
+
+    def handleMissingValue(df):
+        columns = df.columns.to_list()
+        for col in columns:
+            mode = df[col].mode()[0]
+            df[col].fillna(mode, inplace=True)
